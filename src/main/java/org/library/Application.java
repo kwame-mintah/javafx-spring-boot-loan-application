bbfe2fd1a22b37ca1d06aa2e954d74d9a3d3c65e
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2022-03-22 16:36:44 +0000

    refactor(spring): initial springboot application set up
    provide relevant project dependencies and configure application initializr to use `@SpringBootApplication`

---
+++ src/main/java/org/library/Application.java
@@ -0,0 +1,12 @@
+package org.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        // Start the application.
+        SpringApplication.run(Application.class, args);
+    }
+}
