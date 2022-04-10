f2bdc8b071d46b916747df68f1b4a9b35741e089
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2022-04-10 19:38:16 +0100

    feat(scenes): link scenes with controllers
    updated scenes to link with their corresponding controllers, implement navigation for `landing` however currently not working as expected when loading `browse` and `welcome`.

---
+++ src/main/java/org/library/controller/BrowseController.java
@@ -0,0 +1,15 @@
+package org.library.controller;
+
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class BrowseController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //
+    }
+}
---
+++ src/main/java/org/library/controller/LandingController.java
@@ -0,0 +1,51 @@
+package org.library.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import org.library.helper.Navigation;
+import org.library.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class LandingController implements Initializable {
+    @FXML
+    public Button landingScene;
+    @FXML
+    public Button browseScene;
+    @FXML
+    public Button logout;
+
+    @Autowired
+    private GreetingService greetingService;
+
+    private Navigation navigation;
+
+    private static final String WELCOME_FXML_PATH = "/fxml/welcome.fxml";
+    private static final String LANDING_FXML_PATH = "/fxml/landing.fxml";
+    private static final String BROWSE_FXML_PATH = "/fxml/browse.fxml";
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //
+    }
+
+    public void goHome(final ActionEvent actionEvent) throws IOException {
+        navigation.loadNextScene(actionEvent, LANDING_FXML_PATH);
+    }
+
+    public void goBrowse(final ActionEvent actionEvent) throws IOException {
+        navigation.loadNextScene(actionEvent, BROWSE_FXML_PATH);
+    }
+
+    public void goToWelcome(final ActionEvent actionEvent) throws IOException {
+        navigation.loadNextScene(actionEvent, WELCOME_FXML_PATH);
+    }
+}
